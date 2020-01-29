c7cd6c5315d170f3f2a7b29da70d2c8ff6030005
Santiago Guerra <guerrasantiago19@gmail.com>
2020-01-29 12:55:39 -0500

    third exercice

--- chapter-one/random.rb
+++ chapter-one/random.rb
@@ -26,4 +26,21 @@ def array_generate size
   aux_func(size)
 end
 
+# third -> return only even elements
+=begin
+  even_elements( [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] ) ==> [2, 4, 6, 8, 10]
+=end
+def even_elements array
+  def aux_func(array, element, even_elements = [], index = 0)
+    if index >= array.size
+      even_elements
+    else
+      aux_func(array, array[index], even_elements.push(is_even(element) ? element : nil), index + 1)
+    end
+  end
 
+  aux_func(array, array[0]).compact
+end
+
+
+puts even_elements(array_generate(100)).size
